39e2d1afbc19671dff35d2e7e487f432f0202399
Bhavesh Gupta <bhaveshsgupta@gmail.com>
2014-08-08 03:26:36 +0530

    commit
    finally following programming rules atleast few.

--- lexical.c
+++ lexical.c
@@ -1,64 +1,67 @@
-/*Write as program to implement lexical analysis*/
+/*Write as program to implement lexical analysis
+ *Written for the first LAB of the subject Compiler Design
+ * Author@bhaveshsgupta
+ *
+ */
 #include<stdio.h>
 void main()
-{
-        char st[50];
-        int i,a,m;
-        gets(st);
-        for(i=0;st[i]!='\0';i++)
+        {
+            char st[50];
+            int i,_temp;
+            gets(st);
+            for(index=0;st[index]!='\0';index++)
                 {
-
-                        if((st[i]>='a' && st[i]<='z') || (st[m+1]>='A' && st[m+1]<='Z') )
-                                {m=i;
-                                        printf("\n %c",st[i]);
-
-                                        check1:
-                                        if((st[m+1]>='a' && st[m+1]<='z')||(st[m+1]>='A' && st[m+1]<='Z')
- || (st[m+1]>='0' && st[m+1]<='9'))
-                                                {
-                                                m=m+1;
-                                                printf("%c",st[m]);
-                                                goto check1;
-                                                }
-                                        else
-                                                {
-                                                printf(" is indentifer \n");
-                                                i=m;
-                                                }
-                                }
-                        else if(st[i]=='=')
+                    if((st[index]>='a' && st[index]<='z') || (st[_temp+1]>='A' && st[_temp+1]<='Z') )
+                        {
+                            _temp=index;
+                            printf("\n %c",st[index]);
+                            check1:
+                            if((st[_temp+1]>='a' && st[_temp+1]<='z')||(st[_temp+1]>='A' && st[_temp+1]<='Z') || (st[_temp+1]>='0' && st[_temp+1]<='9'))
                                 {
-                                        printf("\n %c is assignment operator \n",st[i]);
+                                    _temp=_temp+1;
+                                    printf("%c",st[_temp]);
+                                    goto check1;
                                 }
-                        else if(st[i]=='*' || st[i]=='/' || st[i]=='-'|| st[i]=='+'|| st[i]=='%')
+                            else
                                 {
-                                        printf("\n %c is arthematic operator \n",st[i]);
+                                    printf(" is indentifer \n");
+                                    index=_temp;
                                 }
-                        else if(st[i]>='0' && st[i]<='9')
-                                { m=i;
-                                        printf("\n %c",st[i]);
-                                        check:
-                                        if(st[m+1]>='0' && st[m+1]<='9')
-                                                {
-                                                m=m+1;
-                                                printf("%c",st[m]);
-                                                goto check;
-                                                }
-                                        else
-                                                {
-                                                printf(" is digit \n");
-                                                i=m;
-                                                }
-
-                                }
-                        else if(st[i]==' ')
-                                {}
-
-
-                        else
+                        }
+                    else if(st[index]=='=')
+                        {
+                            printf("\n %c is assign_ment operator \n",st[index]);
+                        }
+                    else if(st[index]=='*' || st[index]=='/' || st[index]=='-'|| st[index]=='+'|| st[index]=='%')
+                        {
+                            printf("\n %c is arthe_matic operator \n",st[index]);
+                        }
+                    else if(st[index]>='0' && st[index]<='9')
+                        {
+                            _temp=index;
+                            printf("\n %c",st[index]);
+                            check:
+                            if(st[_temp+1]>='0' && st[_temp+1]<='9')
                                 {
-                                        printf("We are developing please wait uptil nxt relase");
+                                    _temp=_temp+1;
+                                    printf("%c",st[_temp]);
+                                    goto check;
                                 }
+                            else
+                                {
+                                    printf(" is digit \n");
+                                    index=_temp;
+                                }
+
+                        }
+                    else if(st[index]==' ')
+                        {
+
+                        }
+                    else
+                        {
+                            printf("We are developing please wait uptil nxt relase");
+                        }
 
 
 
