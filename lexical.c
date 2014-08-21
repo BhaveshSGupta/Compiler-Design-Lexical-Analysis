ff28a05d10016ff242a2425545650f5ee46440ac
Bhavesh Gupta <bhaveshsgupta@gmail.com>
2014-08-21 14:58:18 +0530

    Updates
    Supports all operators.

--- lexical.c
+++ lexical.c
@@ -1,30 +1,29 @@
 /*Write as program to implement lexical analysis
  *Written for the first LAB of the subject Compiler Design
- *This program still analysis small line that too only with few characteristic 
+ *This program still analysis small line that too only with few characterstic 
  * Author@BhaveshSGupta
- *
- */
+*/
 #include<stdio.h>
 #include<stdlib.h>
 void main()
 		{
-            char *string;
+            char *String;
 			int index;
-    		string	= (char *) malloc(100);/*Dynamic allocation for memory management*/
-			printf("Enter the String on which you need to do the Lexical analysis")
-			gets(string);
-            for(index=0;string[index]!='\0';index++)/*The loop to check whether the End of String or Not*/
+    		String	= (char *) malloc(100);/*Dynamic allocation for memory management*/
+			printf("Enter the String on which you need to do the Lexical \n");
+			gets(String);
+            for(index=0;String[index]!='\0';index++)/*The loop to check whether the End of String or Not*/
 				{
 					int _temp;
-					if((string[index]>='a' && string[index]<='z') || (string[_temp]>='A' && string[_temp]<='Z') )/*For Identifier analysis as a rule a identifier should always start with a letter and could consist numbers also*/
+					if((String[index]>='a' && String[index]<='z') || (String[_temp]>='A' && String[_temp]<='Z') )/*For Identifier analysis as a rule a identifier should always start with a letter and could consist numbers also*/
                         {
                             _temp=index;
-                            printf("\n %c",string[index]);
+                            printf("\n %c",String[index]);
                             check1: /*using label */
-                            if((string[_temp+1]>='a' && string[_temp+1]<='z')||(string[_temp+1]>='A' && string[_temp+1]<='Z') || (string[_temp+1]>='0' && string[_temp+1]<='9'))/*To check the next letter in array is alphanumeric or not*/
+                            if((String[_temp+1]>='a' && String[_temp+1]<='z')||(String[_temp+1]>='A' && String[_temp+1]<='Z') || (String[_temp+1]>='0' && String[_temp+1]<='9'))/*To check the next letter in array is alphanumeric or not*/
                                 {
                                     _temp=_temp+1;
-                                    printf("%c",string[_temp]);
+                                    printf("%c",String[_temp]);
                                     goto check1;
                                 }
                             else
@@ -33,23 +32,65 @@ void main()
                                     index=_temp;
                                 }
                         }
-                    else if(string[index]=='=')
+                    else if(String[index]=='=')
                         {
-                            printf("\n %c is Assignment Operator \n",string[index]);
+							if(String[index+1]=='=')
+							{
+							printf("\n == is Relational Operator \n",String[index]);
+							index=index+1;
+							}
+							else 
+							printf("\n %c is Assignment Operator \n",String[index]);
                         }
-                    else if(string[index]=='*' || string[index]=='/' || string[index]=='-'|| string[index]=='+'|| string[index]=='%')
+                    else if(String[index]=='*' || String[index]=='/' || String[index]=='-'|| String[index]=='+'|| String[index]=='%')
                         {
-                            printf("\n %c is Arithmetic Operator \n",string[index]);
-                        }
-                    else if(string[index]>='0' && string[index]<='9')
+							if(String[index]=='+' && String[index+1]=='+')
+							{
+							printf("\n ++ is Arithmetic Increment Operator \n");
+							index=index+1;
+							}
+							else if(String[index]=='+' && String[index+1]=='=')
+							{
+							printf("\n += is Assignment Operator \n");
+							index=index+1;
+							}
+							else if(String[index]=='-' && String[index+1]=='=')
+							{
+							printf("\n -= is Assignment Operator \n");
+							index=index+1;
+							}
+							else if(String[index]=='*' && String[index+1]=='=')
+							{
+							printf("\n *= is Assignment Operator \n");
+							index=index+1;
+							}
+							else if(String[index]=='/' && String[index+1]=='=')
+							{
+							printf("\n /= is Assignment Operator \n");
+							index=index+1;
+							}
+							else if(String[index]=='%' && String[index+1]=='=')
+							{
+							printf("\n %= is Assignment Operator \n");
+							index=index+1;
+							}
+							else if(String[index]=='-' && String[index+1]=='-')
+							{
+							printf("\n -- is Arithmetic Decrement Operator \n");
+							index=index+1;
+							}
+							else
+							printf("\n %c is Arithmetic Operator \n",String[index]);
+						}
+                    else if(String[index]>='0' && String[index]<='9')
                         {
                             _temp=index;
-                            printf("\n %c",string[index]);
+                            printf("\n %c",String[index]);
                             check:
-                            if(string[_temp+1]>='0' && string[_temp+1]<='9')
+                            if(String[_temp+1]>='0' && String[_temp+1]<='9')
                                 {
                                     _temp=_temp+1;
-                                    printf("%c",string[_temp]);
+                                    printf("%c",String[_temp]);
                                     goto check;
                                 }
                             else
@@ -59,11 +100,115 @@ void main()
                                 }
 
                         }
-                    else if(string[index]==' ')
+                    else if(String[index]==' ')
                         {
                         
                         }
-                    else
+                    else if(String[index]=='^')
+                        {
+						if(String[index+1]=='=')
+						{
+						printf("\n ^=  is  bitwise exclusive OR and assignment Operator\n");
+                        index=index+1;
+						}
+						else
+						printf("\n ^  is  BitWise Operator\n");
+                        }
+					else if(String[index]=='~')
+                        {
+                        printf("\n ~  is  BitWise Operator\n");
+                        }
+                    else if(String[index]=='<')
+                        {
+							
+							if(String[index+1]=='=')
+								{
+									printf("<= is relational Operator");
+									index=index+1;
+								}
+							else if(String[index+1]=='<' && String[index+2]=='=')
+								{
+								printf("\n <<= is Left shift AND assignment operator Operator\n");
+									index=index+2;
+								}
+							
+							else if(String[index+1]=='<')
+								{
+									printf("<< is BitWise Left Shift Operator");
+									index=index+1;
+								}
+							else
+								printf("< is relational Operator");
+								
+                        }
+					else if(String[index]=='>')
+                        {
+							if(String[index+1]=='=')
+								{
+									printf(">= is relational Operator");
+									index=index+1;
+								}
+							else if(String[index+1]=='>' && String[index+2]=='=')
+								{
+								printf("\n >>= is Right shift AND assignment operator Operator\n");
+									index=index+2;
+								}
+							else if(String[index+1]=='>')
+								{
+									printf("\n >> is BitWise Left Shift Operator\n");
+									index=index+1;
+								}
+							else
+								printf("> is relational Operator");
+                        }
+					else if(String[index]=='!')
+                        {
+						
+							if(String[index+1]=='=')
+							{
+							printf("\n != is relational Operator");
+							index=index+1;							
+							}
+							else
+							printf(" \n ! is relational Operator\n");
+                        }
+					else if(String[index]=='|')
+                        {
+						
+							if(String[index+1]=='|')
+							{
+							printf("\n|| is relational Operator\n");
+							index=index+1;							
+							}
+							if(String[index+1]=='=')
+							{
+							printf("\n|= is bitwise inclusive OR and assignment Operator\n");
+							index=index+1;							
+							}
+							else
+							printf("\n | is BitWise Operator\n ");							
+							
+                        }
+					else if(String[index]=='&')
+                        {
+						
+							if(String[index+1]=='&')
+							{
+							
+							printf("&& is relational Operator");
+							index=index+1;							
+							}
+							else if(String[index+2]=='=')
+							{
+							
+							printf("&= is Bitwise AND assignment operator Operator");
+							index=index+1;							
+							}
+							else
+							printf("& is BitWise Operator");							
+                        }
+				
+					else
                     {
                         printf("\n We are Developing soon there will be an update");                        
                     }
@@ -71,6 +216,6 @@ void main()
 
 
                 }
-		free(string);
+		free(String);
        
     	}
